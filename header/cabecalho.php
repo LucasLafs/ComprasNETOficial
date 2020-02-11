ce1947fbb94e1f50498dd82217d089f491cabb72
lucaslafs <l.lafs@hotmail.com>
2020-02-11 00:28:17 -0300

    logout

--- header/cabecalho.php
+++ header/cabecalho.php
@@ -88,15 +88,6 @@ else
         <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
           <span class="dropdown-item dropdown-header">15 Notifications</span>
           <div class="dropdown-divider"></div>
-          <!-- <a href="#" class="dropdown-item">
-            <i class="fas fa-envelope mr-2"></i> 4 new messages
-            <span class="float-right text-muted text-sm">3 mins</span>
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <i class="fas fa-users mr-2"></i> 8 friend requests
-            <span class="float-right text-muted text-sm">12 hours</span>
-          </a> -->
           <div class="dropdown-divider"></div>
           <a href="#" class="dropdown-item">
             <i class="fas fa-file mr-2"></i> 3 new reports
@@ -107,8 +98,8 @@ else
         </div>
       </li>
       <li class="nav-item">
-        <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#">
-          <i class="fas fa-th-large"></i>
+          <a href="../apps/logout.php" class="nav-link">
+          <i class="fas fa-sign-out-alt"></i>
         </a>
       </li>
     </ul>
