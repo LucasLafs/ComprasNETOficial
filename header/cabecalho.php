b5fc79f30801c9a9293eeeae84dca8a2fcc6594f
LucasLafs <lucas.lafs@hotmail.com>
2020-02-20 00:50:24 -0300

    RESTART

--- header/cabecalho.php
+++ header/cabecalho.php
@@ -82,28 +82,19 @@ else
       <!-- Notifications Dropdown Menu -->
       <li class="nav-item dropdown">
         <a class="nav-link" data-toggle="dropdown" href="#">
-          <i class="fas fa-cog"></i>
+          <i class="far fa-bell"></i>
+          <span class="badge badge-warning navbar-badge">15</span>
         </a>
         <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-          <span class="dropdown-item dropdown-header">Timeout</span>
+          <span class="dropdown-item dropdown-header">15 Notifications</span>
           <div class="dropdown-divider"></div>
           <div class="dropdown-divider"></div>
           <a href="#" class="dropdown-item">
-            <!-- <i class="fas fa-cog"></i>Buscar Cotações
-            <span class="float-right text-muted text-sm">2 days</span> -->
-            <div class='row'>
-              <form id="form-timout" name="form-timout">
-                <div class='col-md-12'>
-                  <label >Minutos:&nbsp;&nbsp;</label>
-                  <input id='time' type="number" class='input text-center'/>
-                </div>
-              </form>
-            </div>
+            <i class="fas fa-file mr-2"></i> 3 new reports
+            <span class="float-right text-muted text-sm">2 days</span>
           </a>
           <div class="dropdown-divider"></div>
-          <div>
-            <button class="dropdown-item dropdown-footer" onClick='saveTimeout();'>Salvar</button>
-          </div>
+          <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
         </div>
       </li>
       <li class="nav-item">
